a5f14103da37acf99d7203d05f2d6e860b898c6d
wj <wangjun9406@163.com>
2020-03-31 13:32:17 +0800

    initial project

---
+++ src/main/java/com/jam/designpatterns/DesignPatternsCaseApplication.java
@@ -0,0 +1,13 @@
+package com.jam.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jam/designpatternscase/DesignPatternsCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jam.designpatternscase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
